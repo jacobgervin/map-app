9da39bfe40162863f04c0011cc48554a1762aba9
Jacob Gervin <jacobgervin@gmail.com>
2024-08-10 08:53:59 +0200

    location prop

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,19 +1,43 @@
-'use client'
-import Map from 'react-map-gl';
+'use client';
+import React from 'react';
+import Map, { Marker, ViewState } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-export default function LocationMap() {
-    return (
+interface MapProps {
+  location: {
+    lat: number;
+    long: number;
+  };
+}
+
+const LocationMap: React.FC<MapProps> = ({ location }) => {
+    const PaddingOptions = {
+        top: 50,    // 50px padding on top
+        bottom: 50, // 50px padding on bottom
+        left: 50,   // 50px padding on left
+        right: 50,  // 50px padding on right
+      };
+
+  const [viewport, setViewport] = React.useState<ViewState>({
+    longitude: location.long,
+    latitude: location.lat,
+    zoom: 12,
+    bearing: 2,
+    pitch: 1,
+    padding: PaddingOptions
+  });
+
+  return (
     <Map
-        initialViewState={{
-        longitude: 12.284460,
-        latitude: 55.647260,
-        zoom: 6
-        }}
-        style={{width: 600, height: 400}}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
-        mapStyle="mapbox://styles/mapbox/dark-v11"
-    />
-    );
-  }
-  
+      initialViewState={viewport}
+      style={{ width: 600, height: 400 }}
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+      mapStyle="mapbox://styles/mapbox/dark-v11"
+      onMove={(evt) => setViewport(evt.viewState)}
+    >
+      <Marker longitude={location.long} latitude={location.lat} color="red" />
+    </Map>
+  );
+};
+
+export default LocationMap;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
 import LocationMap from "./components/map";
-
+import { useState } from "react";
 export default function Home() {
+
+  const [location, setLocation] = useState({
+    lat: 55.647260, 
+    long: 12.284460,
+  });
+
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LocationMap />
+      <LocationMap location={location}/>
     </main>
   );
 }
